1f0f43ebc93b3bd1fd52478565dd3a60c960d8b4
fickyou <kritzanis0890@gmail.com>
2025-03-13 10:20:41 +0700

    HereKuy

--- mylaravel/app/Http/Controllers/MyController.php
+++ mylaravel/app/Http/Controllers/MyController.php
@@ -6,29 +6,35 @@ use Illuminate\Http\Request;
 
 class MyController extends Controller
 {
-    private function myprivite(){
+    private function myprivate(){
         return 1;
     }
-    function myfunction(Request $req, $var1 = "")
-{
-    $data['myinput'] = $req->input('myinput', '');
-    $data['myvalue'] = "";
-    $data['multiplicationTable'] = [];
-    if (!empty($var1) && preg_match('/^(\d+)$/', $var1, $matches)) {
-        $num1 = (int)$matches[1];
-        $data['myvalue'] = $num1;
-        for ($i = 1; $i <= 12; $i++) {
-            $data['multiplicationTable'][] = "$num1 x $i = " . ($num1 * $i);
-        }
-    } elseif (!empty($data['myinput']) && preg_match('/^(\d+)$/', $data['myinput'], $matches)) {
-        $num1 = (int)$matches[1];
-        $data['myvalue'] = $num1;
+    function myfunction(Request $req , $var1=""){
+        $data['myinput'] = $req->input('myinput', '');
+        $data['myvalue'] = "";
+        $data['multiplicationTable'] = [];
 
-        for ($i = 1; $i <= 12; $i++) {
-            $data['multiplicationTable'][] = "$num1 x $i = " . ($num1 * $i);
+        // ตรวจสอบ $var1 หากไม่ว่าง
+        if (!empty($var1) && preg_match('/^(\d+)\*(\d+)$/', $var1, $matches)) {
+            $num1 = (int)$matches[1];
+            $num2 = (int)$matches[2];
+            $data['myvalue'] = $num1 * $num2;
+
+            // สร้างตารางสูตรคูณ
+            for ($i = 1; $i <= 12; $i++) {
+                $data['multiplicationTable'][] = "$num1 x $i = " . ($num1 * $i);
+            }
+        } elseif (!empty($data['myinput']) && preg_match('/^(\d+)\*(\d+)$/', $data['myinput'], $matches)) {
+            $num1 = (int)$matches[1];
+            $num2 = (int)$matches[2];
+            $data['myvalue'] = $num1 * $num2;
+
+            // สร้างตารางสูตรคูณ
+            for ($i = 1; $i <= 12; $i++) {
+                $data['multiplicationTable'][] = "$num1 x $i = " . ($num1 * $i);
+            }
         }
+
+        return view('myview',$data);
     }
-    return view('myviews', $data);
-}
-
 }
--- mylaravel/routes/web.php
+++ mylaravel/routes/web.php
@@ -10,27 +10,23 @@ use App\Http\Controllers\UserController;
 use App\Http\Controllers\ProductController;
 use App\Http\Middleware\CheckLogin;
 
+Route::middleware([CheckLogin::class])->group(function () {
+    Route::get('/users',[UserController::class, 'index']);
+    Route::get('/user/{id}',[UserController::class, 'edit']);
+    Route::put('/user',[UserController::class, 'edit_action']);
+    Route::delete('/user',[UserController::class, 'delete']);
 
+    Route::get('/product',[ProductController::class, 'index']);
+    Route::post('/product',[ProductController::class, 'add_product']);
 
-Route::middleware([CheckLogin::class])->group(function(){
-    Route :: get('/users',
-    [UserController::class, 'index']);
-    Route :: get('/user/{id}',
-    [UserController::class, 'edit']);
-    Route :: put('/user',
-    [UserController::class, 'edit_action']);
-    Route::delete('/user',
-    [UserController::class, 'delete']);
-
-    Route::get('/product', [ProductController::class, 'index']);
-    Route::post('/product', [ProductController::class, 'add_product']);
-
-    });
+});
 
 Route::get('/',
 [HomeController::class, 'index'])->middleware([CheckLogin::class]);
+
 Route::get('/login',
 [LoginController::class, 'index']);
+
 Route::post('/login',
 [LoginController::class, 'login']);
 
@@ -46,16 +42,14 @@ Route::get('/register',
 Route::get('/home',
 [HomeController::class, 'index']);
 
-Route::get('/',
-[HomeController::class, 'index']);
+// Route::get('/',
+// [HomeController::class, 'index']);
+
 
 Route::post('/register',
 [RegisterController::class, 'create']);
 
-//Route::get('/users',[UserController::class, 'index']);
-//Route::get('/user/{id}',[UserController::class, 'edit']);
-//Route::put('/user',[UserController::class, 'edit_action']);
-//Route::delete('/user',[UserController::class, 'delete']);
+
 
 Route::get('/404', function(){
     abort(404);
